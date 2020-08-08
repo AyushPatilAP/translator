dd37d01feacbd71be8c6c9edbf9ecff50e3edb3e
AyushPatilAP <63277383+AyushPatilAP@users.noreply.github.com>
2020-08-08 10:56:47 +0530

    Update traslator.py

--- traslator.py
+++ traslator.py
@@ -1,8 +1,8 @@
 from translate import Translator
 
 translator=Translator(to_lang='hi')
-with open('yee.txt',mode='r') as my_file:
+with open('#your file',mode='r') as my_file:
 		text=my_file.read()
 		translation=translator.translate(text)
-		with open('test-hi.txt',mode='w') as my_file2:
+		with open('#the new file',mode='w') as my_file2:
 			my_file2.write(translation)
